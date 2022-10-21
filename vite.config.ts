cb4810df276cd067d378e49abba5dfe926941a14
Robbe Vaes <robbevaes95@gmail.com>
2022-10-21 21:01:50 +0200

    Test another

--- vite.config.ts
+++ vite.config.ts
@@ -7,21 +7,11 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, './src/index.ts'),
-      name: 'component-test',
+      name: 'index',
       // the proper extensions will be added
       fileName: format => `index.${format}.js`,
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue',
-        },
-      },
     },
   },
   plugins: [vue()],
