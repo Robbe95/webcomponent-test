3de1cbf20e57378675a35966fd936dc1591b2755
Robbe Vaes <robbevaes95@gmail.com>
2022-10-21 20:20:02 +0200

    Another test

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ export default defineConfig({
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'MyLib',
+      entry: resolve(__dirname, './src/index.ts'),
+      name: 'component-test',
       // the proper extensions will be added
-      fileName: 'my-lib',
+      fileName: format => `${format}.js`,
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
